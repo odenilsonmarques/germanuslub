ed11e79924e6a22327233f80b87796ac89875402
odenilson marques <odmarquesgm@gmail.com>
2025-01-14 22:37:22 -0300

    create section slide

--- front-page.php
+++ front-page.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 
-
 <?php
     get_template_part('parts/content-hero');
     get_template_part('parts/content-services');
     get_template_part('parts/content-contacts');
+    get_template_part('parts/content-slide');
+
 ?>
 
 <?php get_footer(); ?>
